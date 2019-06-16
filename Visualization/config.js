d32e817a642664a49da803fe15d81c7e6e5f586b
Sonja Matejina <sonja.matejina@gmail.com>
2019-06-16 16:13:45 -0700

    commiting updates

--- Visualization/config.js
+++ Visualization/config.js
@@ -1,2 +1,2 @@
 // API key (map) drew's API key, we need to change 
-const API_KEY = "pk.eyJ1IjoiYW5kcmV3aG9hbmcwOSIsImEiOiJjamt1Zno2ejcwNTFkM3FwZGJrOXk1bWxxIn0.BCVeyxRcjhsbOLVqnx5uTQ";
+//const API_KEY = "pk.eyJ1IjoiYW5kcmV3aG9hbmcwOSIsImEiOiJjamt1Zno2ejcwNTFkM3FwZGJrOXk1bWxxIn0.BCVeyxRcjhsbOLVqnx5uTQ";
