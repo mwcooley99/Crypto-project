6b624d950df69b4602266cb612080680202fff03
Matt <mwcooley99@gmail.com>
2019-06-19 08:27:38 -0700

    DB connection working and started Flask structure refactor

---
+++ app.py
@@ -0,0 +1,74 @@
+from flask import Flask, render_template, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+from sqlalchemy import inspect
+from sqlalchemy.ext.automap import automap_base
+
+import os
+# from flask_bootstrap import Bootstrap
+import mysql.connector
+
+import os
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# cnx = mysql.connector.connect(database='heroku_a406bf74b9befa3',
+#                               user=os.getenv('user'),
+#                               password=os.getenv('password'),
+#                               host=os.getenv('host')
+#                               )
+#
+# cursor = cnx.cursor(buffered=True)
+#
+# query = ("SHOW columns FROM top_10_coins")
+# cursor.execute(query)
+# headers = [column[0] for column in cursor.fetchall()]
+#
+# cursor.close()
+# # j = [dict(zip(headers, data) for data in )]
+# cursor = cnx.cursor()
+# query = ("SELECT * FROM top_10_coins")
+# cursor.execute(query)
+# j = [dict(zip(headers, row)) for row in cursor.fetchall()]
+# print(j[0])
+
+# print(cursor)
+
+db = SQLAlchemy(app)
+
+
+# print(db.engine.table_names())
+# Base = automap_base()
+# Base.prepare(db.engine, reflect=True)
+#
+#
+# Crypto = Base.classes.top_10_coins
+# print("*************")
+# db.session.query(Crypto).first()
+
+
+@app.route("/")
+def index():
+    result = db.engine.execute("SHOW columns FROM top_10_coins")
+    headers = [column[0] for column in result.fetchall()]
+
+    # query = db.engine.execute("SELECT * FROM top_10_coins")
+    # j = [dict(zip(headers, row)) for row in query.fetchall()]
+    # print(j[0])
+
+    return str(headers)
+
+
+@app.route("/hello")
+def hello():
+    query = db.engine.execute("SELECT * from top_10_coins LIMIT 1000")
+    j = [row for row in query.fetchall()]
+
+    return str(j)
+
+
+if __name__ == '__main__':
+    app.run()
